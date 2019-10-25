3875058b4cc5844c795551f8ef8bb1f860bd71f3
Keira Hartley <khartley@lcs.on.ca>
2019-10-25 17:24:26 -0400

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Hartley, Keira on 2019-10-25.
+//  Copyright © 2019 Hartley, Keira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
