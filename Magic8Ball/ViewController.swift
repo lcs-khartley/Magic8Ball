cc72805fc1b103c96b5345bf797ac38ff4cd4d9f
Keira Hartley <khartley@lcs.on.ca>
2019-10-26 11:29:29 -0400

    Finished base 8 Ball

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -22,53 +22,59 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-     let randomNumber = Int.random(in: 1...20)
-    
-    switch randomNumber {
-    case 1:
-    print ("It is certain.")
-    case 2:
-    print ("Without a doubt.")
-    case 3:
-    print ("You may rely on it.")
-    case 4:
-    print ("Yes definitely!")
-    case 5:
-    print ("It is decidedly so!")
-    case 6:
-    print ("As I see it, yes")
-    case 7:
-    print ("Most likely")
-    case 8:
-    print ("Yes")
-    case 9:
-    print ("Outlook good")
-    case 10:
-    print ("Signs point to yes")
-    case 11:
-    print ("Reply hazy try again")
-    case 12:
-    print ("Better not tell you now")
-    case 13:
-    print ("Ask again later")
-    case 14:
-    print ("Cannot predict now")
-    case 15:
-    print ("Concentrate and ask again.")
-    case 16:
-    print ("Don’t count on it!")
-    case 17:
-    print ("Outlook not so good.")
-    case 18:
-    print ("My sources say no.")
-    case 19:
-    print ("Very doubtful.")
-    case 20:
-    print ("My reply is no.")
     
     
+    @IBAction func buttonPressed(_ sender: Any) {
+        let randomNumber = Int.random(in: 1...20)
+    
+    
+        switch randomNumber {
+        case 1:
+            labelAnswer.text = "It is certain."
+        case 2:
+            labelAnswer.text = "Without a doubt."
+        case 3:
+            labelAnswer.text = "You may rely on it."
+        case 4:
+            labelAnswer.text = "Yes definitely!"
+        case 5:
+            labelAnswer.text = "It is decidedly so!"
+        case 6:
+            labelAnswer.text = "As I see it, yes"
+        case 7:
+            labelAnswer.text = "Most likely"
+        case 8:
+            labelAnswer.text = "Yes"
+        case 9:
+            labelAnswer.text = "Outlook good"
+        case 10:
+            labelAnswer.text = "Signs point to yes"
+        case 11:
+            labelAnswer.text = "Reply hazy try again"
+        case 12:
+            labelAnswer.text = "Better not tell you now"
+        case 13:
+            labelAnswer.text = "Ask again later"
+        case 14:
+            labelAnswer.text = "Cannot predict now"
+        case 15:
+            labelAnswer.text = "Concentrate and ask again."
+        case 16:
+            labelAnswer.text = "Don’t count on it!"
+        case 17:
+            labelAnswer.text = "Outlook not so good."
+        case 18:
+            labelAnswer.text = "My sources say no."
+        case 19:
+            labelAnswer.text = "Very doubtful."
+        default:
+            labelAnswer.text = "My reply is no."
+            
+            
+            
+        }
     }
+    
     
 
 }
-
